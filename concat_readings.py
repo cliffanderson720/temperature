ad4df66f6b9192bcf83d6c24be81b102757c4f9e
Clifton Anderson <cliff.anderson720@gmail.com>
2021-02-10 18:11:28 -0500

    feat(data): write script to combine multiple readings into one file
    This script isn't very robust: it relies on each df having the same columns.

---
+++ concat_readings.py
@@ -0,0 +1,22 @@
+# Read several temperature files and join into 1 df
+import glob
+import pandas as pd
+
+
+def concat_readings(glob_pattern='2*_temp.csv', write=True):
+    '''
+    :param glob_pattern: shell glob pattern to identify the temperature readings to concatenate
+    :param write: boolean about whether the combined df should be written to temp.csv
+    :return: None
+    '''
+    files = glob.glob(glob_pattern)
+    dfs = [pd.read_csv(file) for file in files]
+    df = pd.concat(dfs)
+    if write:
+        df.to_csv('temp.csv', index=False)
+    return df
+
+
+if __name__ == '__main__':
+    import sys
+    df = concat_readings(sys.argv[1])
