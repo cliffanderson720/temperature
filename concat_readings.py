2f6375dac429cb385324563d0f85d622008ab2ff
Clifton Anderson <cliff.anderson720@gmail.com>
2021-02-20 10:16:26 -0500

    feat(data): add function to get temperature readings from raspberrypi via scp

--- concat_readings.py
+++ concat_readings.py
@@ -1,14 +1,23 @@
 # Read several temperature files and join into 1 df
 import glob
 import pandas as pd
+import subprocess
 
 
-def concat_readings(glob_pattern='2*_temp.csv', write=True):
-    '''
+FILE_GLOB = '2*_temp.csv'
+
+
+def fetch_readings(host='raspberrypi', remote_dir='temperature', local_dir='./'):
+    # fetch temperature readings from raspberrypi/arduino onto the local machine for analysis
+    subprocess.run(['scp', f'{host}:{remote_dir}/{FILE_GLOB}', local_dir])
+
+
+def concat_readings(glob_pattern=FILE_GLOB, write=True):
+    """
     :param glob_pattern: shell glob pattern to identify the temperature readings to concatenate
     :param write: boolean about whether the combined df should be written to temp.csv
     :return: None
-    '''
+    """
     files = glob.glob(glob_pattern)
     dfs = [pd.read_csv(file) for file in files]
     df = pd.concat(dfs)
@@ -18,4 +27,5 @@ def concat_readings(glob_pattern='2*_temp.csv', write=True):
 
 
 if __name__ == '__main__':
+    fetch_readings()
     concat_readings()
